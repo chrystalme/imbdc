75573619009a85215910fd2867f49ac7b5bae05b
Afam Ifeanyi(Chrys) M <affam_ifeanyi@live.com>
2023-01-31 09:45:36 +0200

    Add ratings controller

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,12 @@
 class Rating < ApplicationRecord
   belongs_to :movie
+
+  validates :score, presence: true, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+
+  after_save: update_movie_rating
+
+  private
+  def update_movie_rating
+    movie.update(ratings: movie.ratings.avarage(:score))
+  end
 end
