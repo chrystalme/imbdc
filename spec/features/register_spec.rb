b1efa5d7813aedec0a6999075a2fb2057a6b1e85
Afam Ifeanyi(Chrys) M <affam_ifeanyi@live.com>
2023-02-04 21:27:25 +0200

    Add feature test for signin and register

---
+++ spec/features/register_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "User visits the root of application.", type: :feature do
+  scenario 'Registers a new account' do
+    visit new_user_registration_path
+    fill_in 'email', with: 'test@example.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Confirm password', with: '123456'
+    click_button 'Register'
+
+    expect(page).to have_content("Welcome! You have signed up successfully")
+  end
+  
+  scenario 'Registers a new account' do
+    visit new_user_registration_path
+    fill_in 'email', with: 'test@example.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Confirm password', with: ''
+    click_button 'Register'
+
+    expect(page).to_not have_content("Welcome! You have signed up successfully")
+  end
+  
+  scenario 'Registers a new account' do
+    visit new_user_registration_path
+    fill_in 'email', with: 'test@example.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Confirm password', with: ''
+    click_button 'Register'
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+end
---
+++ spec/features/signIn_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Sign in process", type: :feature do
+  # before :each do
+  #   User.create(name: 'Mary')
+  # end
+  let!(:user){User.create(email: 'tester@test.com', password: '123456', password_confirmation: '123456')}
+  scenario 'user wants to signin with wrong credentials' do
+    visit new_user_session_path
+    fill_in 'email', with: 'test@example.com'
+    fill_in 'Password', with: '123456'
+    click_button 'Sign in'
+
+    expect(page).to_not have_content('Successfully signed In')
+  end
+ 
+  scenario 'user wants to signin with wrong credentials' do
+    visit new_user_session_path
+    fill_in 'email', with: 'tester@example.com'
+    fill_in 'Password', with: '123456'
+    click_button 'Sign in'
+
+    expect(page).to have_content('Invalid Email or password.')
+  end
+  
+  scenario 'user wants to signin with wrong credentials' do
+    visit new_user_session_path
+    fill_in 'email', with: 'tester@test.com'
+    fill_in 'Password', with: '123456'
+    click_button 'Sign in'
+    
+    expect(page).to have_content('Signed in successfully')
+  end
+end
